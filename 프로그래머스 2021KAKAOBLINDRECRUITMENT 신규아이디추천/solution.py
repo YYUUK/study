defb66f5cbcb10d5e4adab9380bb3630cb194eab
YYUUK <aaai0912@naver.com>
2021-06-01 16:34:10 +0900

    pycharm과 git 연동으로 인해 py, md 으로 확장자 변경

--- 프로그래머스 2021KAKAOBLINDRECRUITMENT 신규아이디추천/solution.py
+++ 프로그래머스 2021KAKAOBLINDRECRUITMENT 신규아이디추천/solution.py
@@ -3,49 +3,49 @@ def solution(new_id):
     exc = list('~!@#$%^&*()=+[{]}:?,<>/')
     templist = list(new_id)
     removelist = []
-    #1단계 string 라이브러리의 대문자 소문자 함수
+    # 1단계 string 라이브러리의 대문자 소문자 함수
     for i in range(len(templist)):
         if templist[i].isupper():
             templist[i] = templist[i].lower()
-    #2단계 >> for i in templist 이 구문을 쓰면 도중에 pop, remove되면 커서가 바뀐다... 주의
+    # 2단계 >> for i in templist 이 구문을 쓰면 도중에 pop, remove되면 커서가 바뀐다... 주의
     removecnt = 0
     for i in range(len(templist)):
-        if templist[i-removecnt] in exc:
-            templist.pop(templist.index(templist[i-removecnt]))
+        if templist[i - removecnt] in exc:
+            templist.pop(templist.index(templist[i - removecnt]))
             removecnt = removecnt + 1
-    #3단계 >> i가 같을경우 동일한 인덱스를 두번 저장하는 문제 index()함수는 같은 내용이있으면 가장 앞의 인덱스를 출력
+    # 3단계 >> i가 같을경우 동일한 인덱스를 두번 저장하는 문제 index()함수는 같은 내용이있으면 가장 앞의 인덱스를 출력
     iscomma = 0
     removecnt = 0
     for i in range(len(templist)):
-        if templist[i-removecnt] == '.':
+        if templist[i - removecnt] == '.':
             if iscomma == 1:
-                templist.pop(templist.index(templist[i-removecnt]))
+                templist.pop(templist.index(templist[i - removecnt]))
                 removecnt = removecnt + 1
             else:
-                templist[i-removecnt] = '='
+                templist[i - removecnt] = '='
                 iscomma = 1
         else:
             iscomma = 0
     for i in range(len(templist)):
         if templist[i] == '=':
             templist[i] = '.'
-    #4단계
+    # 4단계
     while templist and templist[0] == '.':
         templist.pop(0)
     while templist and templist[-1] == '.':
         templist.pop()
-    #5단계
+    # 5단계
     if not templist:
         templist.append('a')
-    #6단계
+    # 6단계
     if len(templist) >= 16:
         templist = templist[0:15]
     while templist and templist[-1] == '.':
         templist.pop()
-    #7단계
+    # 7단계
     if len(templist) <= 2:
         while len(templist) < 3:
             templist.append(templist[-1])
     answer = ''.join(templist)
-    
+
     return answer
--- 프로그래머스 SummerWinterCoding(2019) 멀쩡한사각형/solution.py
+++ 프로그래머스 SummerWinterCoding(2019) 멀쩡한사각형/solution.py
@@ -1,4 +1,4 @@
-def solution(w,h):
+def solution(w, h):
     answer = 1
     small = 0
     maxnum = 0
@@ -6,10 +6,10 @@ def solution(w,h):
         small = h
     else:
         small = w
-    
+
     for i in range(small):
         if (w % (i + 1)) == 0 and (h % (i + 1)) == 0:
             maxnum = i + 1
-    
+
     answer = w * h - (w + h - maxnum)
     return answer
